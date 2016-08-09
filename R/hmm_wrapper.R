38be797f6c1d49261c37bae4abb1e8191e76f21d
kasparmartens <kaspar.martens@gmail.com>
2016-08-09 18:51:22 +0100

    postprocessing of output

--- R/hmm_wrapper.R
+++ R/hmm_wrapper.R
@@ -1,6 +1,32 @@
 #' @useDynLib ensembleHMM
 #' @importFrom Rcpp sourceCpp
 
+postprocess_chains = function(res, n_chains_out){
+  m = length(res$trace_x)
+  seq_ind = lapply(1:n_chains_out, function(i)seq(i, m, n_chains_out))
+  res$trace_x = lapply(seq_ind, function(ind) do.call("rbind", res$trace_x[ind]))
+  res$trace_pi = lapply(seq_ind, function(ind) res$trace_pi[ind])
+  res$trace_A = lapply(seq_ind, function(ind) res$trace_A[ind])
+  res$trace_B = lapply(seq_ind, function(ind) res$trace_B[ind])
+  res$switching_prob = lapply(seq_ind, function(ind) do.call("rbind", res$switching_prob[ind]))
+  res$log_posterior = lapply(seq_ind, function(ind) unlist(res$log_posterior[ind]))
+  res$log_posterior_cond = lapply(seq_ind, function(ind) unlist(res$log_posterior_cond[ind]))
+  return(res)
+}
+
+#' @export
+gibbs = function(n_chains, y, k, alpha, max_iter, burnin, which_chains = 1:n_chains, thin = 1, is_fixed_B = FALSE, B = matrix(0, k, s)){
+  n = length(y)
+  s = length(unique(y))
+  if(!all(y %in% 1:s)) stop("y must be an integer from 1 to S")
+  
+  res = ensemble(n_chains, as.integer(y), alpha, k = k, s = s, n = n, max_iter = max_iter, burnin = burnin, thin = thin, estimate_marginals = TRUE, is_fixed_B = is_fixed_B, parallel_tempering = FALSE, crossovers = FALSE, temperatures = rep(1, n_chains), swap_type = 0, swaps_burnin = max_iter, B = B, which_chains = which_chains)
+  
+  # postprocess the traces
+  n_chains_out = length(which_chains)
+  return(postprocess_chains(res, n_chains_out))
+}
+
 #' @export
 crossovers = function(n_chains, y, k, alpha, max_iter, burnin, swaps_burnin, which_chains = 1:n_chains, thin = 1, is_fixed_B = FALSE, B = matrix(0, k, s)){
   n = length(y)
@@ -10,18 +36,8 @@ crossovers = function(n_chains, y, k, alpha, max_iter, burnin, swaps_burnin, whi
   res = ensemble(n_chains, as.integer(y), alpha, k = k, s = s, n = n, max_iter = max_iter, burnin = burnin, thin = thin, estimate_marginals = TRUE, is_fixed_B = is_fixed_B, parallel_tempering = FALSE, crossovers = TRUE, temperatures = rep(1, n_chains), swap_type = 0, swaps_burnin = swaps_burnin, B = B, which_chains = which_chains)
   
   # postprocess the traces
-  m = length(res$trace_x)
   n_chains_out = length(which_chains)
-  seq_ind = lapply(1:n_chains_out, function(i)seq(i, m, n_chains_out))
-  res$trace_x = lapply(seq_ind, function(ind) do.call("rbind", res$trace_x[ind]))
-  res$trace_pi = lapply(seq_ind, function(ind) res$trace_pi[ind])
-  res$trace_A = lapply(seq_ind, function(ind) res$trace_A[ind])
-  res$trace_B = lapply(seq_ind, function(ind) res$trace_B[ind])
-  res$switching_prob = lapply(seq_ind, function(ind) res$switching_prob[ind])
-  res$log_posterior = lapply(seq_ind, function(ind) unlist(res$log_posterior[ind]))
-  res$log_posterior_cond = lapply(seq_ind, function(ind) unlist(res$log_posterior_cond[ind]))
-  
-  return(res)
+  return(postprocess_chains(res, n_chains_out))
 }
 
 #' @export
@@ -36,16 +52,6 @@ parallel_tempering = function(n_chains, temperatures, y, k, alpha, max_iter, bur
                  temperatures = temperatures, swap_type = swap_type, swaps_burnin = swaps_burnin, B = B, which_chains = which_chains)
   
   # postprocess the traces
-  m = length(res$trace_x)
   n_chains_out = length(which_chains)
-  seq_ind = lapply(1:n_chains_out, function(i)seq(i, m, n_chains_out))
-  res$trace_x = lapply(seq_ind, function(ind) do.call("rbind", res$trace_x[ind]))
-  res$trace_pi = lapply(seq_ind, function(ind) res$trace_pi[ind])
-  res$trace_A = lapply(seq_ind, function(ind) res$trace_A[ind])
-  res$trace_B = lapply(seq_ind, function(ind) res$trace_B[ind])
-  res$switching_prob = lapply(seq_ind, function(ind) res$switching_prob[ind])
-  res$log_posterior = lapply(seq_ind, function(ind) unlist(res$log_posterior[ind]))
-  res$log_posterior_cond = lapply(seq_ind, function(ind) unlist(res$log_posterior_cond[ind]))
-  
-  return(res)
+  return(postprocess_chains(res, n_chains_out))
 }
